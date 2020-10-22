7541f852149c47f88ef22c60d7c60907666e2b9c
Freddy Rondon <frondonfermin@gmail.com>
2020-10-22 18:32:01 -0400

    Importing boards from a file

--- Sudoku/SudokuBoard.cs
+++ Sudoku/SudokuBoard.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 
@@ -8,31 +10,37 @@ namespace Sudoku
     class SudokuBoard
     {
         private int[][] board;
+        private int dimension = 9;
         public SudokuBoard()
         {
-            // TODO: Generate random board or passed in as parameter
-
-            /*
-             * Solution:
-             * { 4, 3, 1, 2 }
-             * { 2, 1, 3, 4 }
-             * { 3, 2, 4, 1 }
-             * { 1, 4, 2, 3 }
-             */  
-            board = new int[][]{
-                new int[] { 0, 3, 0, 0 },
-                new int[] { 2, 1, 3, 0 },
-                new int[] { 0, 2, 4, 1 },
-                new int[] { 1, 0, 0, 0 }
-                };
+            string[] lines = File.ReadAllLines(GetRandomFile());
+            board = new int[9][];
+            int i = 0; 
+            foreach (var line in lines)
+            {
+                if (!line.Equals(String.Empty))
+                {
+                    board[i] = line.Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                        .Select(int.Parse)
+                        .ToArray();
+                }
+                i++;
+            }
 
         }
+
+        public string GetRandomFile()
+        {
+            var rand = new Random();
+            var files = Directory.GetFiles("Data", "*.txt");
+            return files[rand.Next(files.Length)];
+        } 
 
         public bool SetCell(int row, int col, int value)
         {
             if (row > board.Length) return false;
             if (col > board[row].Length) return false;
-            if (value > 4 || value < 1) return false;
+            if (value > 9 || value < 1) return false;
             if (!board[row][col].Equals(0)) return false;
 
             board[row][col] = value;
@@ -41,12 +49,12 @@ namespace Sudoku
 
         public bool Verify()
         {
-            for (int i = 0; i < 4; i++)
+            for (int i = 0; i < dimension; i++)
             {
                 var rows = new HashSet<int>();
                 var cols = new HashSet<int>();
                 var box = new HashSet<int>();
-                for (int j = 0; j < 4; j++)
+                for (int j = 0; j < dimension; j++)
                 {
                     // If there are empty cells is not complete
                     if (board[i][j].Equals(0)) return false;
@@ -58,13 +66,13 @@ namespace Sudoku
                     if (!cols.Add(board[j][i])) return false;
 
                     // Boxes in the board
-                    int vertBoxOrder = 2 * (i / 2);
-                    int horzBoxOrder = j / 2;
+                    int vertBoxOrder = 3 * (i / 3);
+                    int horzBoxOrder = j / 3;
                     int boxNo = vertBoxOrder + horzBoxOrder;
 
                     // Cells in the Box
-                    int vertCellOrder = 2 * (i % 2);
-                    int horzCellOrder = j % 2;
+                    int vertCellOrder = 3 * (i % 3);
+                    int horzCellOrder = j % 3;
                     int cellNo = vertCellOrder + horzCellOrder;
 
                     // If there are duplicates in box is invalid
@@ -81,9 +89,12 @@ namespace Sudoku
             var sb = new StringBuilder();
             for (int i = 0; i < board.Length; i++)
             {
+                if (i % 3 == 0) sb.Append("\n");
                 for (int j = 0; j < board[i].Length; j++)
                 {
+                    if (j % 3 == 0) sb.Append(" ");
                     string pos = board[i][j] == 0 ? "_" : board[i][j].ToString();
+                    
                     sb.Append(pos + " ");
                 }
                 sb.AppendLine();
